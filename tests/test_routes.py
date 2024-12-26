c6893c75542bec6d850614e92db3f4736b40ca45
elf0-fr <romain_tirbisch@yahoo.com>
2024-12-26 14:04:56 -0500

    Exercice3:
    WIP: The Get list has to be implemented.
    - Add a test for the delete method.
    - Implement the delete method.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -205,6 +205,23 @@ class TestProductRoutes(TestCase):
         data = response.get_json()
         self.assertIn(f"was not found.", data["message"])
 
+    # ----------------------------------------------------------
+    # TEST DELETE
+    # ----------------------------------------------------------
+    def test_delete_product(self):
+        """It should Delete a Product"""
+        products = self._create_products(5)
+        count = self.get_product_count()
+        test_product = products[0]
+        response = self.client.delete(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(response.data), 0)
+        # make sure they are deleted
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        new_count = self.get_product_count()
+        self.assertEqual(new_count, count - 1)
+
     #
     # ADD YOUR TEST CASES HERE
     #
