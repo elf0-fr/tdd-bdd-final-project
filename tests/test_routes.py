3fe225606a62fb06099404328a449f5dbb92ea25
elf0-fr <romain_tirbisch@yahoo.com>
2024-12-26 14:15:08 -0500

    Exercice 3:
    - add test for get list method.
    - implement get list method.
    
    - The delete method can now be tested.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -180,6 +180,17 @@ class TestProductRoutes(TestCase):
         self.assertIn("was not found", data["message"])
 
     # ----------------------------------------------------------
+    # TEST GET List
+    # ----------------------------------------------------------
+    def test_get_product_list(self):
+        """It should Get a list of Products"""
+        self._create_products(5)
+        response = self.client.get(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), 5)
+
+    # ----------------------------------------------------------
     # TEST UPDATE
     # ----------------------------------------------------------
     def test_update_product(self):
