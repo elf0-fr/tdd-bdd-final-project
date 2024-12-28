43c89848d38ce755bbe6a128f12c71dffa068ace
elf0-fr <romain_tirbisch@yahoo.com>
2024-12-28 08:08:15 -0500

    Exercice 4:
    - fix errors.

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -48,11 +48,11 @@ def step_impl(context):
     #
     for row in context.table:
         payload = {
-            "name": row["name"],
-            "description": row["description"],
-            "price": row["price"],
-            "available": row["available"],
-            "category": row["category"],
+            "name": row['name'],
+            "description": row['description'],
+            "price": row['price'],
+            "available": row['available'] in ['True', 'true', '1'],
+            "category": row['category'],
         }
-        context.rep = requests.post(rest_endpoint, json=payload)
+        context.resp = requests.post(rest_endpoint, json=payload)
         assert context.resp.status_code == HTTP_201_CREATED
